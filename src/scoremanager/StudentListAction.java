302084f6594e1da6d9a2b274d20f334745fdff08
r_ryouta <r_ryouta@LAPTOP-OJ6LGKNR>
2025-05-30 10:24:24 +0900

    h

---
+++ src/scoremanager/StudentListAction.java
@@ -0,0 +1,21 @@
+package scoremanager;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.Student;
+import dao.StudentDao;
+import tool.Action;
+
+public class StudentListAction implements Action {
+
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        StudentDao studentDao = new StudentDao();
+        List<Student> students = studentDao.findAll();
+        request.setAttribute("students", students);
+        request.getRequestDispatcher("/scoremanager/main/student_list.jsp").forward(request, response);
+    }
+}
